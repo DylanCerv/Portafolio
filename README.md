ca3956c301d70cfefc8869ef893cf68642b8c301
Dylan España <gownerbeats@gmail.com>
2022-04-22 18:29:19 -0500

    Portafolio

---
+++ README.md
@@ -0,0 +1,69 @@
+<h1 align="center">Hola, Mi nombre es  Dylan España</h1>
+<h3 align="center">Un fanático de la Programación y de los Negocios</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=DylanCerv&label=Profile%20views&color=0e75b6&style=flat" alt="DylanCerv" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=DylanCerv" alt="DylanCerv" /></a> </p>
+
+- 🔭 Actualmente me encuentro desarrollando en equipo, la plataforma [GesPrender](https://gesprender.com)
+
+- 🌱 En estos momentos me encuentro aprendiento **Data Science, English (Intermediate) and BI**
+
+- 👨‍💻 Aqui pueden encontrar mi portafolio con todos mis proyectos disponibles y poder conocerme un poco más [http://portfolio.gesprender.com](https://portafoliodylangowner.000webhostapp.com/portafolio/index.php#Home)
+
+- 💬 Preguntame sobre **Python, PHP, MySQL, BI**
+
+- 📫 Contacto **gownerbeats@gmail.com**
+
+- 📄 Conoce mis experiencias [CV.pdf](https://www.canva.com/design/DAE0UQV2xlA/mue-sE3FbzDdzJn23b5_zw/view?utm_content=DAE0UQV2xlA&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton)
+
+<h3 align="left">Conectate conmigo</h3>
+<p align="left">
+<a href="https://www.linkedin.com/in/dylan-españa-c-200abc/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="https://www.linkedin.com/in/dylan-españa-c-200abc/" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left">
+    <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40"/>
+    </a>
+    <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/>
+    </a>
+    <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+        <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/>
+    </a>
+    <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/>
+    </a>
+    <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/>
+    </a>
+    <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/>
+    </a>
+    <a href="https://www.php.net" target="_blank" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/>
+    </a>
+    <a href="https://www.python.org" target="_blank" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/>
+    </a>
+    <a href="https://trello.com" target="_blank" rel="noreferrer">
+        <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Trello_icon-icons.com_66775.png" alt="python" width="40" height="40"/>
+    </a>
+    <a href="https://laravel.com" target="_blank" rel="noreferrer">
+        <img src="https://laravel.com/img/logomark.min.svg" alt="python" width="40" height="40"/>
+    </a>
+    <a href="https://getcomposer.org" target="_blank" rel="noreferrer">
+        <img src="https://getcomposer.org/img/logo-composer-transparent4.png" alt="python" width="40" height="40"/>
+    </a>
+    <a href="https://trello.com" target="_blank" rel="noreferrer">
+        <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Trello_icon-icons.com_66775.png" alt="python" width="40" height="40"/>
+    </a>
+    <a href="https://trello.com" target="_blank" rel="noreferrer">
+        <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Trello_icon-icons.com_66775.png" alt="python" width="40" height="40"/>
+    </a>
+</p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=DylanCerv&show_icons=true&locale=en&layout=compact" alt="DylanCerv" /></p>
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=DylanCerv&show_icons=true&locale=en" alt="DylanCerv" /></p>
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=DylanCerv&" alt="DylanCerv" /></p>
---
+++ index.php
@@ -0,0 +1,395 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="es">
+	<head>
+		<title>Portafolio-Dylan</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="icon" href="images/icono-pestaña.ico">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	</head>
+	<body class="is-preload">
+
+			<div class="row">
+				<div class="col-md">
+					<header class="py-" id="Home">
+					</header>
+				</div>
+			</div>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="https://www.linkedin.com/in/dylan-españa-c-200abc/" target="_BLANK" class="logo"><strong>Web Developer</strong> BackEnd</a>
+									<!--
+										https://icon-icons.com/es/icono/linkedin/78667
+									-->
+									<ul class="icons">
+										<li><a href="https://api.whatsapp.com/send?phone=593997237902&text=Hola%20Dylan,%20Soy%20..." target="_BLANK"><img src="images/whatsapp.png" alt="Whatsapp" width="18px"></a></li>
+										<li><a href="https://github.com/DylanCerv" target="_BLANK"><img src="images/GitHub.png" alt="github" width="18px"></a></li>
+										<li><a href="https://www.linkedin.com/in/dylan-españa-c-200abc/" target="_BLANK"><img src="images/LinkedIn.png" alt="LinkedIn" width="18px"></a></li>
+										
+									</ul>
+								</header>
+
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+										<header>
+											<h1>Hola!<br />
+											Soy Dylan España</h1>
+											<i><p>BackEnd</p></i>
+										</header>
+										<p>Desde los 17 años llevo incursionandome en el mundo de la programación, con dedicación aprendi a programar en el area de Desarrollo Web (BackEnd) y he desarrollado diversos proyectos a medida que mis conocimientos han ido creciendo. <br>
+										Tambien actualmente me encuentro aprendiendo diferentes cosas de tecnologia como de emprendimiento, algunas de ellas son: BI, AI, Negociación, Ingles y entre otras.
+										Me considero una persona autodidacta y con un fuerte apego al mundo de los negocios, por este motivo me gusta aprender de todas las areas un poco.
+										</p>
+										<ul class="actions">
+											<!-- <li><a href="https://www.canva.com/design/DAE0UQV2xlA/mue-sE3FbzDdzJn23b5_zw/view?utm_content=DAE0UQV2xlA&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" class="button big" target="_BLANK"><i class="bi bi-file-earmark-pdf-fill"></i>	CV-Español</a></li> -->
+											<li><a href="PDFs/CV-C.pdf" class="button big" target="_BLANK"><i class="bi bi-file-earmark-pdf-fill"></i>	CV-Español</a></li>
+										</ul>
+									</div>
+									<span class="image object">
+										<img src="images/portada-768x432.jpg" alt="portada-768x432" />
+									</span>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2 id="Skills">Skills</h2>
+									</header>
+									<div class="features">
+										<article class="herramienta">
+											<span class="icon">
+												<img src="images/php.png" alt="php" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>PHP</h3>
+												<p>Es un lenguaje de programación que permite el desarrollo web o aplicaciones web dinámicas, el cual es apto para incrustar el lenguaje HTML. Además favorece a la conexión entre el servidor y a la interfaz del usuario.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/mysql.png" alt="mysql" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>MySQL</h3>
+												<p>Un sistema de gestión de bases de datos relacionales, funciona prácticamente en todas las plataformas, Linux, Windows, etc...</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/html.png" alt="html" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>HTML</h3>
+												<p>Es un lenguaje de marcado que nos permite indicar la estructura de nuestro documento mediante etiquetas.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/css.png" alt="css" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>CSS</h3>
+												<p>Es un lenguaje de diseño gráfico para definir y crear la presentación de un documento estructurado escrito en un lenguaje de marcado.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/bootstrap.png" alt="bootstrap" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Bootstrap</h3>
+												<p>Una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/laravel.png" alt="laravel" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Laravel</h3>
+												<p>Es un framework para desarrollar aplicaciones y servicios web con PHP 5, PHP 7 y PHP 8.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/wordpress.png" alt="wordpress" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>WordPress</h3>
+												<p>Es un sistema de gestión de contenidos (CMS) que permite crear y mantener un blog u otro tipo de web.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/python.png" alt="python" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Python</h3>
+												<p>Es un lenguaje de programacion que es ampliamente utilizado por empresas de todo el mundo para construir aplicaciones web, analizar datos, automatizar operaciones y crear aplicaciones empresariales fiables y escalables.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/composer.png" alt="composer" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Composer</h3>
+												<p>Es un sistema de gestión de paquetes para programar en PHP el cual provee los formatos estándar necesarios para manejar dependencias y librerías de PHP.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/Figma.png" alt="composer" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Figma</h3>
+												<p>Figma es un editor de gráficos vectorial y una herramienta de generación de prototipos, principalmente basada en la web, con características off-line adicionales habilitadas por aplicaciones de escritorio en macOS y Windows.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/Trello.png" alt="composer" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Trello</h3>
+												<p>Trello es un software de administración de proyectos con interfaz web y con cliente para iOS y android para organizar proyectos.​</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/Git.png" alt="composer" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Git</h3>
+												<p>Git es un software de control de versiones, pensando en la eficiencia, la confiabilidad y compatibilidad del mantenimiento de versiones de aplicaciones cuando estas tienen un gran número de archivos de código fuente.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon">
+												<img src="images/Google_Colab.png" alt="composer" class="img-fluid">
+											</span>
+											<div class="content">
+												<h3>Google Colab</h3>
+												<p>Colab es un servicio cloud, basado en los Notebooks de Jupyter, que permite el uso gratuito de las GPUs y TPUs de Google, con librerías como: Scikit-learn, PyTorch, TensorFlow, Keras, etc...</p>
+											</div>
+										</article>
+									</div>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2 id="MyProjects">My Projects</h2>
+									</header>
+									<div class="posts">
+										<article>
+											<a href="https://portafoliodylangowner.000webhostapp.com/portafolio/proyectos/CRUD-con-PHP/index.php" class="image" target="_BLANK"><img src="images/CRUD-PHP.JPG" alt="" /></a>
+											<h3>CRUD con PHP</h3>
+											<div class="technologies">
+												<img id="technologies" src="images/php.png" alt="PHP">
+												<img id="technologies" src="images/mysql.png" alt="MySQL">
+												<img id="technologies" src="images/html.png" alt="HTML">
+												<img id="technologies" src="images/bootstrap.png" alt="Booststrap">
+												<img id="technologies" src="images/Git.png" alt="GIT">
+											</div>
+											<p>A modelo de practicar me hice un CRUD para gestionar una lista de personas.</p>
+											<ul class="actions">
+												<li><a href="https://portafoliodylangowner.000webhostapp.com/portafolio/proyectos/CRUD-con-PHP/index.php" target="_BLANK" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="https://portafoliodylangowner.000webhostapp.com/portafolio/proyectos/concesionario-autos/index.php" class="image" target="_BLANK"><img src="images/concesionarioAutos.JPG" alt="" /></a>
+											<h3>Concesionario de Autos</h3>
+											<div class="technologies">
+												<img id="technologies" src="images/php.png" alt="PHP">
+												<img id="technologies" src="images/mysql.png" alt="MySQL">
+												<img id="technologies" src="images/html.png" alt="HTML">
+												<img id="technologies" src="images/css.png" alt="CSS">
+												<img id="technologies" src="images/bootstrap.png" alt="Booststrap">
+												<img id="technologies" src="images/Git.png" alt="GIT">
+											</div>
+											<p>Una Web Pages para poder ver el catalogo de autos de un Concesionario de diversas clases de vehiculos, tanto deportivos como clasicos.</p>
+											<ul class="actions">
+												<li><a href="https://portafoliodylangowner.000webhostapp.com/portafolio/proyectos/concesionario-autos/index.php" target="_BLANK" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="https://drive.google.com/file/d/1eH-HUuJ909eNg6tvDHhcn2NbkF-5juIY/view?usp=sharing" class="image" target="_BLANK"><img src="images/DonRemolo.JPG" alt="DonRemolo" /></a>
+											<h3>Don Remolo</h3>
+											<div class="technologies">
+												<img id="technologies" src="images/php.png" alt="PHP">
+												<img id="technologies" src="images/mysql.png" alt="MySQL">
+												<img id="technologies" src="images/html.png" alt="HTML">
+												<img id="technologies" src="images/css.png" alt="CSS">
+												<img id="technologies" src="images/Git.png" alt="GIT">
+												<img id="technologies" src="images/Javascript.png" alt="Javascript">
+												<img id="technologies" src="images/Figma.png" alt="Figma">
+												<img id="technologies" src="images/Trello.png" alt="Trello">
+											</div>
+											<p>Una pagina web que sirve para pedir pedidos via whatsapp.<br/>
+												NOTA: La pagina web solo se encuentra para movil.
+											</p>
+											<ul class="actions">
+												<li><a href="proyectos/donRemolo/page/onboarding1.html"  target="_BLANK" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="https://gesprender.com/" class="image" target="_BLANK"><img src="images/GesPrender.JPG" alt="" /></a>
+											<h3>GesPrender</h3>
+											<div class="technologies">
+												<img id="technologies" src="images/php.png" alt="PHP">
+												<img id="technologies" src="images/mysql.png" alt="MySQL">
+												<img id="technologies" src="images/html.png" alt="HTML">
+												<img id="technologies" src="images/css.png" alt="CSS">
+												<img id="technologies" src="images/bootstrap.png" alt="Booststrap">
+												<img id="technologies" src="images/Javascript.png" alt="Javascript">
+												<img id="technologies" src="images/Git.png" alt="GIT">
+												<img id="technologies" src="images/Figma.png" alt="Figma">
+												<img id="technologies" src="images/Trello.png" alt="Trello">
+												<img id="technologies" src="images/Postman.png" alt="Postman">
+												<img id="technologies" src="images/Jquery.png" alt="Jquery">
+											</div>
+											<p>Una plataforma para gestion de grandes como pequeños emprendimiento.  (En Mejoras)</p>
+											<ul class="actions">
+												<li><a href="https://gesprender.com/" target="_BLANK" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="https://github.com/DylanCerv/GownerMusic" class="image" target="_BLANK"><img src="images/gownermusic.JPG" alt="" /></a>
+											<h3>Red Social (GownerMusic)</h3>
+											<div class="technologies">
+												<img id="technologies" src="images/php.png" alt="wordpress">
+												<img id="technologies" src="images/mysql.png" alt="wordpress">
+												<img id="technologies" src="images/html.png" alt="wordpress">
+												<img id="technologies" src="images/css.png" alt="wordpress">
+												<img id="technologies" src="images/Git.png" alt="wordpress">
+											</div>
+											<p>Una red social que realize con diferentes tecnologias que esta dedicada a las personas que le gusta la musica como a discograficas que buscan talentos artisticos</p><br>
+											<p>NOTA: Este proyecto aun se encuentra en proceso, pero se puede observar lo avanzando (username: po ---- password: 2)</p>
+											<ul class="actions">
+												<li><a href="https://github.com/DylanCerv/GownerMusic" class="button" target="_BLANK">More</a></li>
+											</ul>
+										</article>
+										
+										
+										
+										
+										
+										
+										
+										
+										<!-- <article>
+											<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+											<h3>Sed etiam facilis</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+											<h3>Feugiat lorem aenean</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+											<h3>Amet varius aliquam</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article> -->
+									</div>
+								</section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="#Home">Home</a></li>
+										<li><a href="#Skills">Skills</a></li>
+										<li><a href="#MyProjects">My Projects</a></li>
+										<li>
+									</ul>
+								</nav>
+
+							<!-- Section -->
+								<section class="skills" id="skills">
+									<header class="major">
+										<h2>Contactame</h2>
+									</header>
+									<p>Puedes enviarme un mensaje, ya sea para encargar un proyecto, para contratarme, sugerir alguna mejora o incluso saludarme. <img id="happy" src="images/happy.png" alt="Happy" width="14px"></p>
+									<ul class="contact">
+										<li class="icon solid fa-envelope"><a href="#">gownerbeats@gmail.com</a></li>
+									</ul>
+									<ul class="contact">
+										<li class="icon solid fa-phone"><a href="#">+593 997237902</a></li>
+									</ul>
+									<!-- <form method="post">
+										<div>
+											<input type="text" name="Nombre" placeholder="Su Nombre" require>
+											<input type="text" name="Email" placeholder="Su Email" require>
+											<input type="text" name="Asunto" placeholder="Asunto" require>
+
+										</div>
+										<textarea name="Mensaje" id="" cols="30" rows="3" placeholder="Mesanje" class="margin-top:5"></textarea>
+										<div>
+											<input type="submit" class="text-center" id="enviar">
+										</div>
+										
+									</form> -->
+								</section>
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">
+										&copy; 2022 Dylan España (Gowner Beats), All Rights Reserved. Plantilla: 
+										<u>
+											<i>
+												<a href="https://html5up.net" target="_blank">HTML5 UP</a>
+											</i>
+										</u>.
+									</p>
+								</footer>
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
