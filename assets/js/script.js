f59dc20823397c24943bbc6a2311777042392231
Dylan <gownerbeats@gmail.com>
2022-09-10 20:10:28 -0500

    update of CVs and Styles of Gif

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var animation = bodymavin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lf20_284fwp5p.json'
+})
