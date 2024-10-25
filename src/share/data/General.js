c4e291a4aa1b33075d0070fe71083179cddee9ea
DYlan <gownerbeats@gmail.com>
2024-10-24 19:27:34 -0500

    new Cv

--- src/share/data/General.js
+++ src/share/data/General.js
@@ -1,5 +1,5 @@
 export const SOBRE_MI = {
-    name: "Dylan España C.",
+    name: "Dylan España Cervantes",
     position: "Desarrollador Full Stack",
     description: `Soy un Desarrollador Web y Mobile Full Stack con +2 años de experiencia, listo para enfrentar desafíos y llevar proyectos emocionantes al siguiente nivel. Mis habilidades incluyen la coordinación de equipos técnicos y un profundo conocimiento en diversas herramientas.`,
     phrase: `¡Vamos juntos hacia el éxito!`
